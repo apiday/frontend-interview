7c11fc8ffaaae2df4ce1eaf92ef047cdde0b5953
Alberto Nebot Oliva <albertonebotoliva@gmail.com>
2024-11-17 22:03:16 +0100

    appBar

--- src/layouts/HomeLayout/HomeLayout.tsx
+++ src/layouts/HomeLayout/HomeLayout.tsx
@@ -4,22 +4,30 @@ import { Questionnaire, questionnaires } from "@/data/questions";
 
 const HomeLayout = () => {
   return (
-    <main className={styles.root}>
-      <h2>Find your ESG surveys. Report. Improve.</h2>
-      <p>
-        <b className={styles.bold}>Boost your ESG performance</b> through
-        tangible tools and actionable insights. 180+ integrations, AI data
-        extraction and ecosystem ESG surveys
-      </p>
-      <section className={styles.bold}>Your surveys:</section>
-      <div className={styles.row}>
-        {questionnaires.map((questionnaire: any) => (
-          <div className={styles.column}>
-            <QuestionnaireCard questionnaire={questionnaire} />
-          </div>
-        ))}
+    <>
+      <div className={styles.appBar}>
+        <img
+          width={100}
+          src="https://cdn.prod.website-files.com/63721a84a9a62db8eb794541/63736a71868f853b9bb0b3d0_logo-apiday_blanc.svg"
+        />
       </div>
-    </main>
+      <main className={styles.root}>
+        <h2>Find your ESG surveys. Report. Improve.</h2>
+        <p>
+          <b className={styles.bold}>Boost your ESG performance</b> through
+          tangible tools and actionable insights. 180+ integrations, AI data
+          extraction and ecosystem ESG surveys
+        </p>
+        <section className={styles.bold}>Your surveys:</section>
+        <div className={styles.row}>
+          {questionnaires.map((questionnaire: any) => (
+            <div className={styles.column}>
+              <QuestionnaireCard questionnaire={questionnaire} />
+            </div>
+          ))}
+        </div>
+      </main>
+    </>
   );
 };
 
